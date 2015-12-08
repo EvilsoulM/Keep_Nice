70886d6105f0e4d03c0300c3179217366dd2212b
mazixuan <mazixuan@meituan.com>
2015-12-08 19:26:18 +0800

    fix

---
+++ app/src/debug/java/com/evilsoulm/keep_nice/KeepDebugApplication.java
@@ -0,0 +1,10 @@
+package com.evilsoulm.keep_nice;
+
+/**
+ * Author by mazixuan
+ * Data:2015-12-08 19:24
+ * Project:Keep_Nice
+ * Detail:
+ */
+public class KeepDebugApplication extends KeepApplication {
+}
