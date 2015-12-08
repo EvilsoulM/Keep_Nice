1eba96dd7e691bd23ebf5cbb36d1e52e310dfa08
mazixuan <mazixuan@meituan.com>
2015-12-08 17:48:22 +0800

    change project->ext

---
+++ app/src/main/java/com/evilsoulm/keep_nice/KeepApplication.java
@@ -0,0 +1,12 @@
+package com.evilsoulm.keep_nice;
+
+import android.app.Application;
+
+/**
+ * Author by mazixuan
+ * Data:2015-12-08 17:26
+ * Project:Keep_Nice
+ * Detail:
+ */
+public class KeepApplication extends Application {
+}
