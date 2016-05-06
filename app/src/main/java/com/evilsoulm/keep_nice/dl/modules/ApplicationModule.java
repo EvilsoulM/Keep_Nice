6b237ca0e453f7c68ebd76b94160de056f0e59e4
马梓轩 <mazixuan@meituan.com>
2016-05-06 15:51:09 +0800

    update okhttp inject

--- app/src/main/java/com/evilsoulm/keep_nice/dl/modules/ApplicationModule.java
+++ app/src/main/java/com/evilsoulm/keep_nice/dl/modules/ApplicationModule.java
@@ -1,21 +1,13 @@
 package com.evilsoulm.keep_nice.dl.modules;
 
-import android.accounts.AccountManager;
 import android.content.Context;
 
 import com.evilsoulm.keep_nice.common.KeepApplication;
-import com.evilsoulm.keep_nice.net.OkHttpCacheInterceptor;
-import com.evilsoulm.keep_nice.net.OkhttpLoggingInterceptor;
-
-import java.io.File;
-import java.util.concurrent.TimeUnit;
 
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
-import okhttp3.Cache;
-import okhttp3.OkHttpClient;
 
 /**
  * Author by mazixuan
@@ -35,48 +27,5 @@ public class ApplicationModule {
     @Singleton
     Context provideApplicationContext() {
         return this.application;
-    }
-
-    @Provides
-    @Singleton
-    AccountManager provideAccountManager(Context context) {
-        return AccountManager.get(context);
-    }
-
-    @Provides
-    @Singleton
-    OkHttpClient provideOkhttp(Context context, Cache cache) {
-        OkHttpClient.Builder client = new OkHttpClient().newBuilder();
-        OkHttpCacheInterceptor okHttpCacheInterceptor = new OkHttpCacheInterceptor();
-
-        client.networkInterceptors().add(okHttpCacheInterceptor);
-        client.interceptors().add(okHttpCacheInterceptor);
-        client.interceptors().add(new OkhttpLoggingInterceptor());
-        client.connectTimeout(30, TimeUnit.SECONDS);
-        client.readTimeout(30, TimeUnit.SECONDS);
-        client.writeTimeout(30, TimeUnit.SECONDS);
-
-        if (cache != null) {
-            client.cache(cache);
-        }
-
-        return client.build();
-    }
-
-    @Provides
-    @Singleton
-    Cache provideCache(Context context) {
-
-        //设置缓存
-        File httpCacheDirectory = new File(context.getCacheDir(), "responses");
-
-        Cache cache = null;
-        try {
-            cache = new Cache(httpCacheDirectory, 10 * 1024 * 1024);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return cache;
     }
 }
--- app/src/main/java/com/evilsoulm/keep_nice/dl/modules/NetModule.java
+++ app/src/main/java/com/evilsoulm/keep_nice/dl/modules/NetModule.java
@@ -1,18 +1,24 @@
 package com.evilsoulm.keep_nice.dl.modules;
 
+import android.accounts.AccountManager;
 import android.app.Application;
+import android.content.Context;
 
 import com.bumptech.glide.Glide;
 import com.evilsoulm.keep_nice.common.ImageLoader;
+import com.evilsoulm.keep_nice.net.OkHttpCacheInterceptor;
+import com.evilsoulm.keep_nice.net.OkhttpLoggingInterceptor;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.Cache;
+import okhttp3.OkHttpClient;
 
-/**
- * Created by gaofei on 15-11-12.
- */
 @Module
 public class NetModule {
 
@@ -20,5 +26,48 @@ public class NetModule {
     @Singleton
     ImageLoader provideImageLoader(Application movieProApplication) {
         return new ImageLoader(Glide.with(movieProApplication));
+    }
+
+    @Provides
+    @Singleton
+    AccountManager provideAccountManager(Context context) {
+        return AccountManager.get(context);
+    }
+
+    @Provides
+    @Singleton
+    OkHttpClient provideOkhttp(Context context, Cache cache) {
+        OkHttpClient.Builder client = new OkHttpClient().newBuilder();
+        OkHttpCacheInterceptor okHttpCacheInterceptor = new OkHttpCacheInterceptor();
+
+        client.networkInterceptors().add(okHttpCacheInterceptor);
+        client.interceptors().add(okHttpCacheInterceptor);
+        client.interceptors().add(new OkhttpLoggingInterceptor());
+        client.connectTimeout(30, TimeUnit.SECONDS);
+        client.readTimeout(30, TimeUnit.SECONDS);
+        client.writeTimeout(30, TimeUnit.SECONDS);
+
+        if (cache != null) {
+            client.cache(cache);
+        }
+
+        return client.build();
+    }
+
+    @Provides
+    @Singleton
+    Cache provideCache(Context context) {
+
+        //设置缓存
+        File httpCacheDirectory = new File(context.getCacheDir(), "responses");
+
+        Cache cache = null;
+        try {
+            cache = new Cache(httpCacheDirectory, 10 * 1024 * 1024);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return cache;
     }
 }
