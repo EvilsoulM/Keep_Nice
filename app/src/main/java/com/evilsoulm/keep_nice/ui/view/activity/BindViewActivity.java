105fa09b9cace0a9e9ad68f93b61de1ca09b1c17
马梓轩 <mazixuan@meituan.com>
2016-08-21 21:40:55 +0800

    observer

--- app/src/main/java/com/evilsoulm/keep_nice/ui/view/activity/BindViewActivity.java
+++ app/src/main/java/com/evilsoulm/keep_nice/ui/view/activity/BindViewActivity.java
@@ -2,9 +2,12 @@ package com.evilsoulm.keep_nice.ui.view.activity;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.evilsoulm.keep_nice.R;
 import com.evilsoulm.keep_nice.databinding.ActivityBindViewBinding;
@@ -17,19 +20,19 @@ public class BindViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         long startTime = System.currentTimeMillis();
-//        setContentView(R.layout.activity_bind_view);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        TextView tv1 = (TextView) findViewById(R.id.tv1);
-//        TextView tv2 = (TextView) findViewById(R.id.tv2);
-//        TextView tv3 = (TextView) findViewById(R.id.tv3);
-//        TextView tv4 = (TextView) findViewById(R.id.tv4);
-//        TextView tv5 = (TextView) findViewById(R.id.tv5);
-//        TextView tv6 = (TextView) findViewById(R.id.tv6);
-//        TextView tv7 = (TextView) findViewById(R.id.tv7);
-//        TextView tv8 = (TextView) findViewById(R.id.tv8);
-//        TextView tv9 = (TextView) findViewById(R.id.tv9);
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        Log.d("EvilsoulM", "costTime:" + (System.currentTimeMillis() - startTime));
+        setContentView(R.layout.activity_bind_view);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        TextView tv1 = (TextView) findViewById(R.id.tv1);
+        TextView tv2 = (TextView) findViewById(R.id.tv2);
+        TextView tv3 = (TextView) findViewById(R.id.tv3);
+        TextView tv4 = (TextView) findViewById(R.id.tv4);
+        TextView tv5 = (TextView) findViewById(R.id.tv5);
+        TextView tv6 = (TextView) findViewById(R.id.tv6);
+        TextView tv7 = (TextView) findViewById(R.id.tv7);
+        TextView tv8 = (TextView) findViewById(R.id.tv8);
+        TextView tv9 = (TextView) findViewById(R.id.tv9);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        Log.d("EvilsoulM", "costTime:" + (System.currentTimeMillis() - startTime));
 
         long bindTime = System.currentTimeMillis();
         ActivityBindViewBinding activityBindViewBinding = DataBindingUtil.setContentView(this, R.layout.activity_bind_view);
